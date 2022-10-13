1679c815f9994a4ad5f45058519c4c7d8700880a
unknown <devashishkaushik0@gmail.com>
2022-10-13 20:41:08 +0530

    Updated and improved code structure in preparation for enabling user-customizable blocking. Manifest.json now hasnew permissions, notably storage. The background script now uses chrome.storage to store the full set of websites when the extension is installed. It then calls an asynchronous listner by passing a message. This listener retreives the current set of stored URL's from memory. It removes the previous blocker (if any) and then blocks the websites as before. This architecture enables dynamic changing of the filter list by message passing.

--- background.js
+++ background.js
@@ -1,9 +1,44 @@
-var callback = function(details) {
+// A callback function which returns the webpage which replaces blocked sites
+//Used by the blocking API call to webRequest.onBeforeRequest
+function blockRequest(details) {
     return {redirectUrl: "chrome-extension://kgeeojleeblggkaeloloinflodojcnad/sagemode.html"};
-};
+}
 
-var filter = { urls: ["*://*.facebook.com/*", "*://*.youtube.com/*", "*://*.twitter.com/*", "*://*.reddit.com/*", "*://*.netflix.com/*", "*://*.4chan.org/*", "*://*.twitch.tv/*","*://*.instagram.com/*"]};
-var opt_extraInfoSpec = ["blocking"];
+var defaultFilter = { urls: ["*://*.facebook.com/*", "*://*.youtube.com/*", "*://*.twitter.com/*", "*://*.reddit.com/*", "*://*.netflix.com/*", "*://*.4chan.org/*", "*://*.twitch.tv/*","*://*.instagram.com/*"]};
 
-chrome.webRequest.onBeforeRequest.addListener(
-        callback, filter, opt_extraInfoSpec);
+
+  chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+    alert(response);
+    // console.log(response)
+    alert("Message received from popup")
+    if ("URLUPDATE" == response) {
+        chrome.storage.local.get( ['urls'], function (cache) {
+            alert("Trying to retreive urls");
+            //alert(cache.urls)
+            //console.log(defaultFilter)
+            //console.log(cache)
+            //console.log(cache.urls)
+            //console.log(cache.urls==defaultFilter.urls)
+            //alert((cache==defaultFilter))
+
+            if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest)) {
+                chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+            }
+
+            chrome.webRequest.onBeforeRequest.addListener(
+                blockRequest, cache, ["blocking"]);
+        }
+        );
+    }
+  });
+
+  // Runs when extension is first installed
+  // Basically to do some housekeeping and setup work
+  chrome.runtime.onInstalled.addListener(function(details){ // The event can also be triggered by an update
+    if(details.reason == "install"){ // The reason attribiute allows us to know for sure
+        chrome.storage.local.set(defaultFilter , function() { // Saves the default filter with all 8 sites blocked to storage
+            chrome.runtime.sendMessage("URLUPDATE"); // Sends a message which will trigger the listener looking for filter modification
+            // This will start the default blocking listener until the user makes any further changes via the popup
+        });
+    }
+});
