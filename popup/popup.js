c2fa2f7d9f015d4578f9a0ec03e570cd24341078
unknown <devashishkaushik0@gmail.com>
2022-10-14 02:48:14 +0530

    Updated extension version. Completed code documentation. Added bootstrap JS link to popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,10 @@
 // Initialize the filter list in the popup with current values
 window.onload = function() { // Runs everytime popup is opened
+    console.log("Popup opened")
     chrome.storage.local.get( ['urls'], function (cache) {
-        if (!cache.urls.includes("*://*.facebook.com/*")) {
-            document.getElementById("facebook").checked = false;
+        console.log("Blocked URL list from local storage : ", cache.urls)
+        if (!cache.urls.includes("*://*.facebook.com/*")) { // if the URL list in local storage does NOT include the URL
+            document.getElementById("facebook").checked = false; // Then uncheck the corresponding check box
         }
         if (!cache.urls.includes("*://*.youtube.com/*")) {
             document.getElementById("youtube").checked = false;
@@ -26,65 +28,55 @@ window.onload = function() { // Runs everytime popup is opened
             document.getElementById("instagram").checked = false;
         }
     }
-    );
+    ); // Thus, only the websites which are blocked are ticked
 }
 
 
 // Update the block list as per the checkboxes selected by the user
 // All sites are checked when popup opens for first time as specified in html
-// After that previous selection is shon on opening using window.onload above
+// After that previous selection is shown on opening using window.onload above
 document.getElementById('blocking').onclick = async () => { // Run whenever the block button in the popup is clicked
     userResponse = window.prompt("Please type \"I am responsible for this\"")
+    console.log("New blocking list entered by user. Confirmation requested")
 
-    if ("I am responsible for this" == userResponse) {
-        var urlList = new Array();
+    if ("I am responsible for this" == userResponse) { // Check if user entered correct phrase
+        console.log("Confirmation successful")
+
+        var urlList = new Array(); // If yes, put the new list of URLs which are to be blocked in an array
         urlList.push("*://prohibited.illegal.domain/*")
-        //urlList = ["*://*.facebook.com/*", "*://*.youtube.com/*", "*://*.twitter.com/*", "*://*.reddit.com/*", "*://*.netflix.com/*", "*://*.4chan.org/*", "*://*.twitch.tv/*","*://*.instagram.com/*"]
         if (document.getElementById('facebook').checked ) {
             urlList.push("*://*.facebook.com/*")
-            //const index = urlList.indexOf("*://*.facebook.com/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('youtube').checked) {
             urlList.push("*://*.youtube.com/*")
-            //const index = urlList.indexOf("*://*.youtube.com/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('twitter').checked) {
             urlList.push("*://*.twitter.com/*")
-            //const index = urlList.indexOf("*://*.twitter.com/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('reddit').checked) {
             urlList.push("*://*.reddit.com/*")
-            //const index = urlList.indexOf("*://*.reddit.com/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('netflix').checked) {
             urlList.push("*://*.netflix.com/*")
-            //const index = urlList.indexOf("*://*.netflix.com/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('4chan').checked) {
             urlList.push("*://*.4chan.org/*")
-            //const index = urlList.indexOf("*://*.4chan.org/*");
-            //.splice(index, 1);
         }
         if (document.getElementById('twitch').checked) {
             urlList.push("*://*.twitch.tv/*")
-            //const index = urlList.indexOf("*://*.twitch.tv/*");
-            //urlList.splice(index, 1);
         }
         if (document.getElementById('instagram').checked) {
             urlList.push("*://*.instagram.com/*")
-            //const index = urlList.indexOf("*://*.instagram.com/*");
-            //urlList.splice(index, 1);
         }
+        console.log("URLsdesignated to be blocked : ", urlList)
 
-        chrome.storage.local.set({ "urls": urlList }, function() {
-        //alert("Saved succesfully");
-        alert('The following blocks were set :  ' + urlList.slice(1));
-        chrome.runtime.sendMessage("URLUPDATE");
+        chrome.storage.local.set({ "urls": urlList }, function() { // Save the list in local storage
+            console.log("New list of URLs saved succesfully")
+            alert('The following blocks were set :  ' + urlList.slice(1));
+            chrome.runtime.sendMessage("URLUPDATE");  // Send message saying URLUPDATE which will be picked up by background script
+            // This will serve as a signal to update the listener which is blocking the URLs with the new list
         });
+    } else {
+        console.log("Confirmation failed")
     }
 };
